97c9714bfb9a66c2703a7c7e7740085cb2257784
Jacob Carlborg <doob@me.com>
2024-06-22 14:43:38 +0200

    Implement getString

--- source/dxc/utilities/plist.d
+++ source/dxc/utilities/plist.d
@@ -6,7 +6,8 @@ import dxml.dom;
 
 struct Plist
 {
-    private DOMEntity!string root;
+    private alias Element = DOMEntity!string;
+    private Element root;
 
     static Plist parse(string data)
     {
@@ -16,11 +17,14 @@ struct Plist
         return Plist(rootDict);
     }
 
-    int getInteger(string key) =>
-        root.children[indexOfKey(key) + 1].children.front.text.to!int;
+    int getInteger(string key) => getValueFor(key).to!int;
+    string getString(string key) => getValueFor(key);
 
     private size_t indexOfKey(string key) =>
         root
             .children
             .countUntil!(e => e.name == "key" && e.children.front.text == key);
+
+    private string getValueFor(string key) =>
+        root.children[indexOfKey(key) + 1].children.front.text;
 }
--- tests/dxc/utilities/plist.d
+++ tests/dxc/utilities/plist.d
@@ -14,3 +14,18 @@ XML";
     auto plist = Plist.parse(data);
     assert(plist.getInteger("Foo") == 2);
 }
+
+@"getString" unittest
+{
+    const data = q"XML
+        <plist version="1.0">
+          <dict>
+            <key>Foo</key>
+            <string>bar</string>
+          </dict>
+        </plist>
+XML";
+
+    auto plist = Plist.parse(data);
+    assert(plist.getString("Foo") == "bar");
+}
